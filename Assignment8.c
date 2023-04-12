ea7b9fe60e43996b2e6cc0db481b07d20c8b078d
ShauryaKAJLA <126277304+ShauryaKAJLA@users.noreply.github.com>
2023-04-12 13:23:02 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -2,7 +2,32 @@
 //input: Anitha
 //output: Aahint 
 
+#include<stdio.h>
+#include<string.h>
 int main()
 {
-  return 0;
+	int i,len=0,j=0,fl=0,o,count;
+	char str[50];
+	char ch1='A',ch2='a',sm;
+	printf("Enter a string: ");
+	gets(str);
+	for(;ch1<='Z';ch1++)
+	{
+		for(i=j;str[i]!='\0';i++)
+		{
+	     if(ch1==str[i]||ch2==str[i])
+	      {
+	    	fl=i;
+	    	sm=str[i];
+	    	for(o=fl;o>=j;o--)
+	    	{
+	    		str[o]=str[o-1];
+			}
+			str[j]=sm;
+			j++;
+		  }
+	    }
+	    ch2++;
+	}
+	puts(str);
 }
