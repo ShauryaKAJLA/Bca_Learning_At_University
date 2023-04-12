66d26311366da217f02265f034ef370cd4c52ec0
ShauryaKAJLA <126277304+ShauryaKAJLA@users.noreply.github.com>
2023-04-12 13:38:26 +0530

    Update Assignment9.c

--- Assignment9.c
+++ Assignment9.c
@@ -4,9 +4,39 @@
 //.       a = 2
 //.       m = 1
 //.       n = 1
-
+#include<stdio.h>
+#include<string.h>
 int main()
 {
-  
-  return 0;
+	int count,i;
+	char str[50],ch,ch1;
+	printf("Enter a string: ");
+	gets(str);
+	for(ch='A',ch1='a';ch<='Z';ch1++,ch++)
+	{
+		count=0;
+		for(i=0;str[i]!='\0';i++)
+		{
+			if(str[i]==ch)
+			{
+				count=count+1;
+			}
+		}
+		if(count>0)
+		{
+			printf("%c: %d\n",ch,count);
+		}
+		count=0;
+		for(i=0;str[i]!='\0';i++)
+		{
+			if(str[i]==ch1)
+			{
+				count=count+1;
+			}
+		}
+		if(count>0)
+		{
+			printf("%c: %d\n",ch1,count);
+		}
+	}
 }
