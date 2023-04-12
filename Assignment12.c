cb0de9c2bed526708408d1489bbd276f88bd5c52
ShauryaKAJLA <126277304+ShauryaKAJLA@users.noreply.github.com>
2023-04-12 14:00:09 +0530

    Update Assignment12.c

--- Assignment12.c
+++ Assignment12.c
@@ -1,6 +1,22 @@
 //Input marks of 10 students in an array and then find the student with maximum marks
-
+#include<stdio.h>
 int main()
 {
-  return 0;
+	int i,ary[50],j,greater,fl=0;
+	printf("Enter number of elements: ");
+	scanf("%d",&i);
+	for(j=0;j<i;j++)
+	{
+		scanf("%d",&ary[j]);
+	}
+	greater=ary[0];
+	for(j=1;j<i;j++)
+	{
+		if(ary[j]>greater)
+		{
+			greater=ary[j];
+			fl=j;
+		}
+	}
+	printf("Student %d got higest marks: %d",++fl,greater);
 }
