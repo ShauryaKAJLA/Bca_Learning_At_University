ddd33daa5555096c3939aec006b6c7d25b80e117
ShauryaKAJLA <126277304+ShauryaKAJLA@users.noreply.github.com>
2023-04-27 21:51:20 +0530

    Update Assignment11.c

--- Assignment11.c
+++ Assignment11.c
@@ -1,8 +1,54 @@
 //Convert an decimal to its binary equivalent using a bitwise operator
 
-int main()
+i#include<stdio.h>
+
+#include<conio.h>
+
+ 
+
+void binary(unsigned int);   
+
+ 
+
+void main()
+
 {
-  int N = 7;
-  
-  return 0;
+
+unsigned int num;
+ 
+printf("Enter Decimal Number : ");
+
+scanf("%u",&num);
+
+binary(num);   
+
+getch();
+
+}
+
+
+void binary(unsigned int num)
+
+{
+
+unsigned int mask=32768;   
+printf("Binary Eqivalent : ");
+
+ 
+
+while(mask > 0)
+
+   {
+
+   if((num & mask) == 0 )
+
+         printf("0");
+
+   else
+
+         printf("1");
+
+  mask = mask >> 1 ;  
+   }
+
 }
